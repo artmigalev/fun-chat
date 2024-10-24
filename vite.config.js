e667fdc0286a3917cb1f28269e9ad75fc3639f07
artmigalev <art_migalev@yahoo.com>
2024-10-24 19:15:09 +0300

    start project

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths"; // Плагин для поддержки путей из tsconfig.json
 import eslintPlugin from "vite-plugin-eslint";
 import svgr from "vite-plugin-svgr";
+
+
 export default defineConfig({
     root: "./",
-    base: "fun-chat",
-    mode: "development",
+    base: "/fun-chat/",
     server: {
         // port: 3000,
         port: 4000,
@@ -13,23 +14,15 @@ export default defineConfig({
         hmr: true,
     },
     build: {
-        outDir: "./public",
+        outDir: "dist",
         sourcemap: true,
     },
-    resolve: {
-        alias: {
-            "@": "/src",
-            "@component": "/src/app/components/**",
-            "@utils": "/src/app/utils/**",
-            "@home": "/src/pages/home/**",
-            "@pages": "/src/pages/**",
-        },
-    },
+
     plugins: [
         tsconfigPaths(),
         eslintPlugin(),
         svgr({
-            exportAsDefault: true,
+            exportAsDefault: false,
             svgo: true, // Оптимизация SVG
             svgoConfig: {
                 plugins: [
